814f14ce683ba3ae2e0e40aaac3e4f45bf3733db
kevinblack <kevin.black@3dsystems.com>
2018-07-23 22:23:54 -0700

    Add where and when section and begin accommodation section

--- _site/js/main.js
+++ _site/js/main.js
@@ -3,8 +3,17 @@
 (function($) {
     $(document).ready(function() {
         $('.image-slider').slick({
-        	lazyLoad: 'ondemand',
-        	fade: true
+            lazyLoad: 'ondemand',
+            fade: true
         });
+
+        $('.attire-slider').slick({
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            centerMode: true,
+            variableWidth: true
+        });
+
     });
 })(jQuery);
